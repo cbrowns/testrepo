89b269f638aee0a46ca97b42dee764de3b1089c1
Cristian <cabss0929@gmail.com>
2019-04-24 23:26:46 -0400

    test commit

--- backEnd/src/articles/api/views.py
+++ backEnd/src/articles/api/views.py
@@ -1,15 +1,43 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from articles.models import Article
 
 from .serializers import ArticleSerializers
 
+from rest_framework import viewsets
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
     serializer_class = ArticleSerializers
-
-
-class ArticleDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializers
+
+
+# from rest_framework.generics import (
+#     ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView)
+
+
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+
+# class ArticleUpdateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
+
+
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializers
